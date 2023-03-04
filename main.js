96286771f125fd3bec9c0ab459caee72629098a3
Rudra Pratap Singh <65117190+Ryder-07@users.noreply.github.com>
2023-03-04 23:19:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,21 +3,33 @@ let increment=1;
 document.getElementById("plus").addEventListener("click", () =>{
     count=count+increment;
     console.log(count);
-    document.getElementById("num").innerHTML=count
+    document.getElementById("num").innerHTML=count;
+    color();
 })
 document.getElementById("minus").addEventListener("click", () =>{
     count=count-increment;
     console.log(count);
-    document.getElementById("num").innerHTML=count
+    document.getElementById("num").innerHTML=count;
+    color();
 })
 document.getElementById("reset").addEventListener("click", () =>{
     count=0;
     increment=1;
     console.log(count);
-    document.getElementById("num").innerHTML=count
+    document.getElementById("num").innerHTML=count;
+    color();
 })
 document.getElementById("increment").addEventListener("click", () =>{
     increment=increment+1;
     console.log(count);
-    document.getElementById("num").innerHTML=count
+    document.getElementById("num").innerHTML=count;
+    color();
 })
+function color(){
+if(count<0){
+    document.getElementById("num").style.color= "#eb3461";
+}
+else if(count>=0){
+    document.getElementById("num").style.color= "rgb(109,81,232)";
+}
+}
